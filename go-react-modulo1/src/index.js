290be8b4d746e1afecd3f1c67898f0aee08f1ca3
Sérgio Neto <sergio.deusdedith@gmail.com>
2019-08-01 19:32:47 -0300

    mexendo com onClick

--- go-react-modulo1/src/index.js
+++ go-react-modulo1/src/index.js
@@ -3,16 +3,30 @@ import { render } from "react-dom";
 
 class Button extends Component {
   render() {
-    return <a href="#">Salvar</a>;
+    return (
+      <a href="#" onClick={this.props.onClick}>
+        {this.props.children}
+      </a>
+    );
   }
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { clicks: 0 };
+  }
+
+  handleClick(e) {
+    this.setState(() => this.state.clicks++);
+  }
+
   render() {
     return (
       <Fragment>
-        <h1>Hehehe kkkk</h1>
-        <Button />
+        <h1>Hello Rocketseat</h1>
+        <h2>O botão foi clicado {this.state.clicks} vezes.</h2>
+        <Button onClick={this.handleClick.bind(this)}>Salvaaaaaar</Button>
       </Fragment>
     );
   }
