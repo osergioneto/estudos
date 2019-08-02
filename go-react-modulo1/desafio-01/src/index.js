680948e6fd144adc00d3730e90ffc313567873ab
Sérgio Neto <sergio.deusdedith@gmail.com>
2019-08-02 10:50:24 -0300

    Header component

--- go-react-modulo1/desafio-01/src/index.js
+++ go-react-modulo1/desafio-01/src/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 
+import Header from "./js/components/Header";
+import "./reset.css";
+
 class App extends Component {
   render() {
-    return <h1>Hello</h1>;
+    return <Header />;
   }
 }
 
---
+++ go-react-modulo1/desafio-01/src/js/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./style.css";
+
+const Header = props => {
+  return (
+    <header id="header">
+      <h1>RocketBook</h1>
+    </header>
+  );
+};
+
+export default Header;
