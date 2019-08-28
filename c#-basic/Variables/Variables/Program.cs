03cd001b201d26fc8f2786375dd098c8d47d07a2
Sergio Neto <Sergio Neto@DESKTOP-9S1S376>
2019-08-28 15:42:37 -0300

    Variables

---
+++ c#-basic/Variables/Variables/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte numero = 2;
+            int inteiro = 230;
+            float pontoFlutuante = 123.456f;
+            bool boolean = false;
+            char character = 'A';
+            string primeiroNome = "Sérgio";
+
+            Console.WriteLine("byte: {0}", numero);
+            Console.WriteLine("int: {0}", inteiro);
+            Console.WriteLine("float: {0}", pontoFlutuante);
+            Console.WriteLine("bool: {0}", boolean);
+            Console.WriteLine("char: {0}", character);
+            Console.WriteLine("string: {0}", primeiroNome);
+
+            const float Pi = 3.14f;
+            Console.WriteLine("Pi is {0}", Pi);
+
+            Console.WriteLine("Min and Max - Bool: {0} {1}", byte.MinValue, byte.MaxValue);
+        }
+    }
+}
