5fc39f916b6c41b4971b4657a065bb1b717d4be6
Sergio Neto <sergio.deusdedith@gmail.com>
2019-08-29 01:30:06 -0300

    Operator and minor changes

--- c#-basic/TYpeConversion/TYpeConversion/Program.cs
+++ c#-basic/TYpeConversion/TYpeConversion/Program.cs
@@ -10,6 +10,41 @@ namespace TYpeConversion
     {
         static void Main(string[] args)
         {
+            byte b1 = 1;
+            int i1 = b1;
+            Console.WriteLine("byte: {0}", i1);
+
+            int i2 = 1;
+            byte b2 = (byte)i2;
+            Console.WriteLine("int: {0}", b2);
+
+            int i3 = 1000;
+            byte b3 = (byte) i3;
+            Console.WriteLine("int: {0}", b3);
+
+            try
+            {
+                string number = "1234";
+                byte b = Convert.ToByte(number);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to byte");
+            }
+
+            try
+            {
+                string trueText = "true";
+                bool boolean = Convert.ToBoolean(trueText);
+                Console.WriteLine(boolean);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The text could not be converted to boolean");
+            }
+
+
         }
     }
 }
