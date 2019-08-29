b17e6637818c0d4b41fcc82a24e1cf79ddc40a32
Sergio Neto <sergio.deusdedith@gmail.com>
2019-08-29 03:07:26 -0300

    Arrays

--- c#-basic/Non-primitive-types/Non-primitive-types/Program.cs
+++ c#-basic/Non-primitive-types/Non-primitive-types/Program.cs
@@ -14,6 +14,21 @@ namespace Non_primitive_types
             Dog.Talk();
 
             System.Console.WriteLine(Calculator.Add(gaia.getAge(), diana.getAge()));
+
+            // Arrays
+
+            var numbers = new int[3];
+            numbers[0] = 1;
+
+            System.Console.WriteLine(numbers[0]);
+            System.Console.WriteLine(numbers[1]);
+            System.Console.WriteLine(numbers[2]);
+
+            var names = new string[3] { "Arya", "Gaia", "Diana" };
+
+            System.Console.WriteLine(names[0]);
+            System.Console.WriteLine(names[1]);
+            System.Console.WriteLine(names[2]);
         }
     }
 }
