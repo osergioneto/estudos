2263ca83452b0be635bb1467917b101d153f79d2
Sérgio Neto <sergio.deusdedith@cubos.io>
2019-09-13 14:38:06 -0300

    Feature: Add subscription and refactor of Mutation, Query and others

--- hackernews-node/src/resolvers/Query.js
+++ hackernews-node/src/resolvers/Query.js
@@ -2,8 +2,32 @@ function info() {
     return `This is the API of Hackernews clone.`
 }
 
-function feed(parent, args, context, info) {
-    return context.prisma.links();
+async function feed(parent, args, context, info) {
+    const where = args.filter ? {
+        OR: [
+            { description_contais: args.fitler },
+            { url_contains: args.filter },
+        ],
+    } : {};
+
+    const links = await context.prisma.links({
+        where,
+        skip: args.skip,
+        first: args.first,
+        orderBy: args.orderBy
+    });
+
+    const count = await context.prisma
+        .linksConnection({
+            where,
+        })
+        .aggregate()
+        .count();
+
+    return {
+        links,
+        count,
+    };
 }
 
 function link(parent, args, context, info) {
