7bd36405444d3adc66996086e36f5975cc021690
Sérgio Neto <sergio.deusdedith@cubos.io>
2020-04-07 00:02:32 -0300

    study: iterators study

---
+++ console-log-my-food/index.js
@@ -0,0 +1,105 @@
+#! /usr/bin/env node
+const axios = require("axios");
+
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+readline.prompt();
+readline.on('line', async line => {
+    switch(line.trim()) {
+        case "list vegan foods": 
+            {
+              axios.get("http://localhost:3001/foods").then(({data}) => {
+                let idx = 0;
+                const veganOnly = data.filter(food => {
+                  return food.dietary_preferences.includes("vegan");
+                });
+                const veganIterable = {
+                  [Symbol.iterator]() {
+                    return {
+                      [Symbol.iterator]() { return this; },
+                      next() {
+                        const current = veganOnly[idx];
+                        idx++;
+                        if (current) {
+                          return { value: current, done: false }
+                        } else {
+                          return { value: current, done: true  }
+                        }
+                      }
+                    }
+                  }
+                };
+                for (const val of veganIterable) {
+                  console.log(val.name);
+                }
+                readline.prompt();
+              });
+            }
+            break;
+        case "log":
+            {
+              const { data } = await axios.get("http://localhost:3001/foods");
+              const it = data[Symbol.iterator]();
+              let actionIt;
+            
+              const actionIterator = {
+                [Symbol.iterator]() {
+                  let positions = [...this.actions];
+                  return {
+                    [Symbol.iterator]() {
+                      return this;
+                    },
+                    next(...args) { 
+                      if(positions.length > 0) {
+                        const position = positions.shift();
+                        const result = position(...args);
+                        return { value: result, done: false };
+                      } else {
+                        return { done: true };
+                      }
+                    }
+                  }
+                },
+                actions: [askForServingSize, displayCalories]
+              };
+
+              function askForServingSize(food) {
+                readline.question(`How many servings did you eat? (as a decimal: 1, 0.25, 1.25, etc...)`, servingSize => {
+                  actionIt.next(servingSize, food);
+                });
+              }
+
+              function displayCalories(servingSize, food) {
+                const calories = food.calories;
+                console.log(`
+                  ${
+                    food.name
+                  } with a serving size of ${servingSize} has a ${Number.parseFloat(
+                    calories * parseInt(servingSize, 10),
+                  ).toFixed()} calories. `
+                );
+                actionIt.next();
+                readline.prompt();
+              }
+
+              readline.question(`What would you like to log today? `,  async item => {
+                    let position = it.next();
+                    while(!position.done) {
+                        const food = position.value.name;
+                        if (food === item) {
+                            console.log(`${item} has ${position.value.calories} calories`);
+                            actionIt = actionIterator[Symbol.iterator]();
+                            actionIt.next(position.value);
+                        }
+                        position = it.next();
+                    }
+                    console.log(item);
+                    readline.prompt();
+                });
+            }
+    }
+});
+
