816fcdfeea16e5752b6f535e62794c40302cf6e0
Sérgio Neto <sergio.deusdedith@cubos.io>
2020-04-19 05:52:19 -0300

    feat: add build_grid function

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -16,5 +16,20 @@ defmodule Identicon do
   def pick_color(%Identicon.Image{ hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | rgb: {r, g, b} }
   end
+
+  def build_grid(%Identicon.Image{ hex: hex } = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{ image | grid: grid }
+  end
+
+  def mirror_row([first, second, third] = _row) do
+    [first, second, third, second, first]
+  end
   end
 end
