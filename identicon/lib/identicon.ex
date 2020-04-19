27330e5a1505ccc1a3f7fb519001d54e926681f7
Sérgio Neto <sergio.deusdedith@cubos.io>
2020-04-19 00:50:53 -0300

    feat: add main and hash_input functions

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,4 +3,12 @@ defmodule Identicon do
   Documentation for `Identicon`.
   """
 
+  def main(input) do
+    input
+    |> hash_input
+  end
+
+  def hash_input(input) do
+    :crypto.hash(:md5, input) |> :binary.bin_to_list
+  end
 end
