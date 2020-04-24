919b4b98e92b531913cf1d0ab01c5a5204506e3a
Sérgio Neto <sergio.deusdedith@cubos.io>
2020-04-23 23:36:09 -0300

    feat: add crud operations

--- learn-node/simple-express/src/index.js
+++ learn-node/simple-express/src/index.js
@@ -1,44 +1,57 @@
 const express = require("express");
+const { uuid } = require("uuidv4");
 
 const app = express();
+app.use(express.json());
+
+const projects = [];
 
 app.get("/projects", (req, res) => {
-  console.log(req.body);
-  return res.status(200).json([
-    "Project 1",
-    "Project 2",
-    "Project 3",
-  ]);
+  return res.status(200).json(projects);
 });
 
 app.post("/projects", (req, res) => {
-  console.log(req.body);
-  return res.status(201).json([
-    "Project 1",
-    "Project 2",
-    "Project 3",
-    "Project 4",
-  ]);
+  const { title, owner } = req.body;
+
+  const project = { id: uuid(), title, owner };
+  projects.push(projects);
+
+  return res.status(201).json(project);
 });
 
 app.put("/projects/:id", (req, res) => {
-  console.log(req.body);
-  res.status(200)
-  return res.status(200).json([
-    "Project 1",
-    "Project 2",
-    "Project 3",
-    "Project 5",
-  ]);
+  const { id } = req.params;
+  const { title, owner } = req.body;
+  
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return res.status(400).json({ message: "Project not found"});
+  }
+
+  const project = {
+    id,
+    title,
+    owner
+  };
+
+  projects[projectIndex] = project;
+
+  return res.status(200).json(project);
 });
 
 app.delete("/projects/:id", (req, res) => {
-  console.log(req.body);
-  return res.status(200).json([
-    "Project 1",
-    "Project 2",
-    "Project 3",
-  ])
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return res.status(400).json({ error: "Project not found" });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return res.status(204).send();
 });
 
 app.listen(4000 || process.env.PORT, () => {
