6b7ab69b5a413d197f28c9def6b20b0e42a12c40
Sérgio Neto <sergio.deusdedith@cubos.io>
2020-05-17 20:55:46 -0300

    study; add solution to challenge 4

--- javascript-the-hard-parts-v2/promises_async_and_the_event_loop.js
+++ javascript-the-hard-parts-v2/promises_async_and_the_event_loop.js
@@ -34,7 +34,7 @@ function helloGoodbye() {
 /* CHALLENGE 4 */
 
 function brokenRecord() {
-  // ADD CODE HERE
+  setInterval(() => console.log("hi again"), 1000);
 }
 // Uncomment the following line to check your work!
 // brokenRecord(); // should log (every second): hi again
