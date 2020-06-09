8d7c09190f126142ebb6e6675b243fbf372338d4
Sérgio Neto <sergio.deusdedith@cubos.io>
2020-06-08 21:02:11 -0300

    chore: add prettier and eslint

--- react-studies/github-explorer/src/App.tsx
+++ react-studies/github-explorer/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-  
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World <span role="img" aria-label="eye mouth eye">👁️👄👁️</span></h1>
+      <h1>
+        Hello World
+        <span role="img" aria-label="eye mouth eye">
+          👁️👄👁️💅
+        </span>
+      </h1>
     </div>
   );
 }
