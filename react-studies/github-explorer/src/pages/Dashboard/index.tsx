eefb14c189802652566392dc0c4b91ef31d72861
Sérgio Neto <sergio.deusdedith@cubos.io>
2020-06-09 22:19:33 -0300

    feat: add dashboard and repository pages

---
+++ react-studies/github-explorer/src/pages/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return (
+    <h1>
+      Hello World
+      <span role="img" aria-label="eye mouth eye">
+        👁️👄👁️💅
+      </span>
+    </h1>
+  );
+};
+
+export default Dashboard;
