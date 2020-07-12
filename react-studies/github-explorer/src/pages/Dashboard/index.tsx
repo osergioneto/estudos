79428d363e75f51c799ca698092501a83cd6850a
Sérgio Neto <sergio.deusdedith@gmail.com>
2020-07-12 15:42:42 -0300

    feat: add form and repositories to Dashboard

--- react-studies/github-explorer/src/pages/Dashboard/index.tsx
+++ react-studies/github-explorer/src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -9,10 +10,56 @@ const Dashboard: React.FC = () => {
       <img src={logoImg} alt="Github Explorer" />
       <Title>
         Explore repositórios no GitHub
+        <br />
         <span role="img" aria-label="eye mouth eye">
           👁️👄👁️💅
         </span>
       </Title>
+      <Form>
+        <input type="text" placeholder="Digite aqui" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="https://github.com/osergioneto/estudos">
+          <img
+            src="https://avatars3.githubusercontent.com/u/17184941?s=460&u=232106eb88002ee3d9dfc94f47de969815573abb&v=4"
+            alt="Sérgio Neto"
+          />
+          <div>
+            <strong>osergioneto/estudos</strong>
+            <p>Repositório para concentrar os estudos que venho fazendo.</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="https://github.com/osergioneto/estudos">
+          <img
+            src="https://avatars3.githubusercontent.com/u/17184941?s=460&u=232106eb88002ee3d9dfc94f47de969815573abb&v=4"
+            alt="Sérgio Neto"
+          />
+          <div>
+            <strong>osergioneto/estudos</strong>
+            <p>Repositório para concentrar os estudos que venho fazendo.</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="https://github.com/osergioneto/estudos">
+          <img
+            src="https://avatars3.githubusercontent.com/u/17184941?s=460&u=232106eb88002ee3d9dfc94f47de969815573abb&v=4"
+            alt="Sérgio Neto"
+          />
+          <div>
+            <strong>osergioneto/estudos</strong>
+            <p>Repositório para concentrar os estudos que venho fazendo.</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
