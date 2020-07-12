d3b55f84ba4732f7dfe202e00663b51e122335f9
Sérgio Neto <sergio.deusdedith@gmail.com>
2020-07-12 19:21:57 -0300

    feat: fetch repositories using axios

--- react-studies/github-explorer/src/pages/Dashboard/index.tsx
+++ react-studies/github-explorer/src/pages/Dashboard/index.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+/* eslint-disable camelcase */
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import { Title, Form, Repositories } from './styles';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
+interface IRepository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<IRepository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const { data: repository } = await api.get(`/repos/${newRepo}`);
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
@@ -15,50 +38,28 @@ const Dashboard: React.FC = () => {
           👁️👄👁️💅
         </span>
       </Title>
-      <Form>
-        <input type="text" placeholder="Digite aqui" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          type="text"
+          placeholder="Digite aqui"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="https://github.com/osergioneto/estudos">
-          <img
-            src="https://avatars3.githubusercontent.com/u/17184941?s=460&u=232106eb88002ee3d9dfc94f47de969815573abb&v=4"
-            alt="Sérgio Neto"
-          />
-          <div>
-            <strong>osergioneto/estudos</strong>
-            <p>Repositório para concentrar os estudos que venho fazendo.</p>
-          </div>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="#">
+            <img src={repository.owner.avatar_url} alt="Sérgio Neto" />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="https://github.com/osergioneto/estudos">
-          <img
-            src="https://avatars3.githubusercontent.com/u/17184941?s=460&u=232106eb88002ee3d9dfc94f47de969815573abb&v=4"
-            alt="Sérgio Neto"
-          />
-          <div>
-            <strong>osergioneto/estudos</strong>
-            <p>Repositório para concentrar os estudos que venho fazendo.</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="https://github.com/osergioneto/estudos">
-          <img
-            src="https://avatars3.githubusercontent.com/u/17184941?s=460&u=232106eb88002ee3d9dfc94f47de969815573abb&v=4"
-            alt="Sérgio Neto"
-          />
-          <div>
-            <strong>osergioneto/estudos</strong>
-            <p>Repositório para concentrar os estudos que venho fazendo.</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
