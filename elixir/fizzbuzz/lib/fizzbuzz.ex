45027e328ed74e8de09e5dcf6b0328cc5c8294f8
Sérgio Deusdedith de Araujo Neto <sergio.deusdedith@gmail.com>
2020-10-06 20:54:28 -0300

    refactor: add file erro handling. separate convert and evaluate into function

--- elixir/fizzbuzz/lib/fizzbuzz.ex
+++ elixir/fizzbuzz/lib/fizzbuzz.ex
@@ -12,17 +12,27 @@ defmodule Fizzbuzz do
       :world
 
   """
-  def run({:ok, array}) do
-    array
-    |> String.split(",")
-    |> Enum.map(fn item
-      -> String.trim(item)
-      |> String.to_integer()
-      |> check_fizzbuzz()
-    end)
+  def build(file_name) do
+    file_name
+    |> File.read()
+    |> handle_file_read()
   end
 
-  def check_fizzbuzz(num) when is_integer(num) do
+  defp handle_file_read({:ok, array}) do
+    array
+    |> String.split(",")
+    |> Enum.map(fn item -> item |> convert_and_evaluate() end)
+  end
+
+  defp handle_file_read({:error, reason}), do: {:error, "Error reading the file: #{reason}"}
+
+  defp convert_and_evaluate(elem) do
+    String.trim(elem)
+    |> String.to_integer()
+    |> check_fizzbuzz()
+  end
+
+  defp check_fizzbuzz(num) when is_integer(num) do
     cond do
       divisible_by_15(num) -> "FizzBuzz"
       divisible_by_5(num) -> "Buzz"
@@ -31,7 +41,7 @@ defmodule Fizzbuzz do
     end
   end
 
-  def divisible_by_3(num) when is_integer(num), do: rem(num, 3) === 0
-  def divisible_by_5(num) when is_integer(num), do: rem(num, 5) === 0
-  def divisible_by_15(num) when is_integer(num), do: rem(num, 3) === 0 and rem(num, 5) === 0
+  defp divisible_by_3(num) when is_integer(num), do: rem(num, 3) === 0
+  defp divisible_by_5(num) when is_integer(num), do: rem(num, 5) === 0
+  defp divisible_by_15(num) when is_integer(num), do: rem(num, 3) === 0 and rem(num, 5) === 0
 end
