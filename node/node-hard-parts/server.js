b472cc0fc813ae4b076e7b32cd5da5ece33af920
Sérgio Deusdedith de Araujo Neto <sergio.deusdedith@gmail.com>
2020-12-19 23:59:40 -0300

    study: challenge

--- node/node-hard-parts/server.js
+++ node/node-hard-parts/server.js
@@ -1,32 +1,37 @@
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs/promises');
+const path = require('path');
 
-function doOnRequest(request, response){
+async function doOnRequest(request, response) {
   // Send back a message saying "Welcome to Twitter"
   // code here...
-  response.end("yo")  
   if (request.method === 'GET' && request.url === '/') {
     // read the index.html file and send it back to the client
     // code here...
-    fs.createReadStream()
-
+    const file = await fs.readFile(path.join("index.html"));
+    console.log("file: ", file.toString());
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(file);
+    response.end();
   }
   else if (request.method === 'POST' && request.url === '/sayHi') {
-    // code here...
-    
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end("hi back to you!");
   }
   else if (request.method === 'POST' && request.url === '/greeting') {
     // accumulate the request body in a series of chunks
     // code here...
-    
+
   }
   else {
     // Handle 404 error: page not found
     // code here...
-    
+
   }
 }
 
 const server = http.createServer(doOnRequest)
 
-server.listen(3000);
+server.listen(3000, () => {
+  console.log("Running server 🚀");
+});
