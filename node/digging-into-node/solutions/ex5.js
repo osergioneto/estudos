e662e18c35428d56226828694d1a883ad22ff331
Sérgio Deusdedith de Araujo Neto <sergio.deusdedith@gmail.com>
2021-01-04 21:27:52 -0300

    study: creating a webserver

--- node/digging-into-node/solutions/ex5.js
+++ node/digging-into-node/solutions/ex5.js
@@ -12,8 +12,8 @@ var staticAlias = require("node-static-alias");
 
 // ************************************
 
-const DB_PATH = path.join(__dirname,"my.db");
-const WEB_PATH = path.join(__dirname,"web");
+const DB_PATH = path.join(__dirname, "my.db");
+const WEB_PATH = path.join(__dirname, "web");
 const HTTP_PORT = 8039;
 
 var delay = util.promisify(setTimeout);
@@ -23,8 +23,8 @@ var delay = util.promisify(setTimeout);
 var myDB = new sqlite3.Database(DB_PATH);
 var SQL3 = {
 	run(...args) {
-		return new Promise(function c(resolve,reject){
-			myDB.run(...args,function onResult(err){
+		return new Promise(function c(resolve, reject) {
+			myDB.run(...args, function onResult(err) {
 				if (err) reject(err);
 				else resolve(this);
 			});
@@ -35,7 +35,7 @@ var SQL3 = {
 	exec: util.promisify(myDB.exec.bind(myDB)),
 };
 
-var fileServer = new staticAlias.Server(WEB_PATH,{
+var fileServer = new staticAlias.Server(WEB_PATH, {
 	cache: 100,
 	serverInfo: "Node Workshop: ex5",
 	alias: [
@@ -74,20 +74,20 @@ function main() {
 	console.log(`Listening on http://localhost:${HTTP_PORT}...`);
 }
 
-async function handleRequest(req,res) {
+async function handleRequest(req, res) {
 	if (/\/get-records\b/.test(req.url)) {
 		await delay(1000);
 
 		let records = await getAllRecords() || [];
 
-		res.writeHead(200,{
+		res.writeHead(200, {
 			"Content-Type": "application/json",
 			"Cache-Control": "max-age: 0, no-cache",
 		});
 		res.end(JSON.stringify(records));
 	}
 	else {
-		fileServer.serve(req,res);
+		fileServer.serve(req, res);
 	}
 }
 
@@ -95,32 +95,32 @@ async function handleRequest(req,res) {
 // NOTE: if sqlite3 is not working for you,
 //   comment this version out
 // *************************
-async function getAllRecords() {
-	var result = await SQL3.all(
-		`
-		SELECT
-			Something.data AS "something",
-			Other.data AS "other"
-		FROM
-			Something
-			JOIN Other ON (Something.otherID = Other.id)
-		ORDER BY
-			Other.id DESC, Something.data
-		`
-	);
+// async function getAllRecords() {
+// 	var result = await SQL3.all(
+// 		`
+// 		SELECT
+// 			Something.data AS "something",
+// 			Other.data AS "other"
+// 		FROM
+// 			Something
+// 			JOIN Other ON (Something.otherID = Other.id)
+// 		ORDER BY
+// 			Other.id DESC, Something.data
+// 		`
+// 	);
 
-	return result;
-}
+// 	return result;
+// }
 
 // *************************
 // NOTE: uncomment and use this version if
 //   sqlite3 is not working for you
 // *************************
-// async function getAllRecords() {
-// 	// fake DB results returned
-// 	return [
-// 		{ something: 53988400, other: "hello" },
-// 		{ something: 342383991, other: "hello" },
-// 		{ something: 7367746, other: "world" },
-// 	];
-// }
+async function getAllRecords() {
+	// fake DB results returned
+	return [
+		{ something: 53988400, other: "hello" },
+		{ something: 342383991, other: "hello" },
+		{ something: 7367746, other: "world" },
+	];
+}
