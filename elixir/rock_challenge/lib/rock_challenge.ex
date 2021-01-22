38c0fdfe3d00a58bbf65b70fc572cbec81e96fac
Sérgio Deusdedith de Araujo Neto <sergio.deusdedith@gmail.com>
2021-01-22 19:20:31 +0000

    feat: add function to split value and merge values

--- elixir/rock_challenge/lib/rock_challenge.ex
+++ elixir/rock_challenge/lib/rock_challenge.ex
@@ -12,22 +12,22 @@ defmodule RockChallenge do
       :world
 
   """
-  def share_payments(shopping_list) do
-    shopping_items =
+  def share_payments(shopping_list, buyers_emails) do
+    total =
       shopping_list
         |> File.read()
         |> handle_file_read()
         |> sum_prices()
-        |> IO.inspect()
-    
-    # buyers_emails
-    #  |> handle_file_read(buyers_list)
-    :hello
+
+    buyers_emails
+      |> File.read()
+      |> handle_file_read()
+      |> split_values(total)
   end
 
   defp sum_prices(items) do
     { sums, total } = 
-    items
+      items
         |> Enum.map_reduce(0, 
           fn item,
             acc -> {item["amount"] * item["price"], 
@@ -36,6 +36,21 @@ defmodule RockChallenge do
 
     total
   end
+
+  defp split_values(buyers, total) do
+    total 
+    |> div_and_mod(Enum.count(buyers))
+    |> merge_values(buyers)
+  end
+
+  defp merge_values({quotient, reminder}, buyers) do
+    splited_values = List.duplicate(quotient, Enum.count(buyers))
+
+    buyers
+    |> Enum.zip(List.insert_at(splited_values,0, (quotient + reminder)))
+    |> Enum.into(%{})
+  end
+
   defp div_and_mod(divident, divisor) do
     quotient = div(divident, divisor)
     reminder = rem(divident, divisor)
