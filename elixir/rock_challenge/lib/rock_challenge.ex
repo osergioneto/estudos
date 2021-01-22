ef416069d687652b213fb9b06e6209eba4fdda9b
Sérgio Deusdedith de Araujo Neto <sergio.deusdedith@gmail.com>
2021-01-22 13:54:44 +0000

    feat: add function to sum all prices

--- elixir/rock_challenge/lib/rock_challenge.ex
+++ elixir/rock_challenge/lib/rock_challenge.ex
@@ -12,14 +12,23 @@ defmodule RockChallenge do
       :world
 
   """
-  def hello do
-    :world
+  def share_payments(shopping_list) do
+    shopping_items =
+      shopping_list
+        |> File.read()
+        |> handle_file_read()
+        |> sum_prices()
+        |> IO.inspect()
+    
+    # buyers_emails
+    #  |> handle_file_read(buyers_list)
+    :hello
   end
 
-  def sum_prices(file_name) do
-    file_name
-    |> File.read()
-    |> handle_file_read()
+  defp sum_prices({:ok, items}) do
+    IO.inspect(items)
+    items
+    |> Enum.map_reduce(0, fn item, acc -> {item["amount"] * item["price"], item["amount"] * item["price"] + acc} end)
   end
 
   defp handle_file_read({ok, array}) do
