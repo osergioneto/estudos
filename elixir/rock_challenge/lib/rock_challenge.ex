ebb17d78416f96bdcb39c33dc7b717fa95fb239a
Sérgio Deusdedith de Araujo Neto <sergio.deusdedith@gmail.com>
2021-01-22 00:09:01 -0300

    feat: add handle file function

--- elixir/rock_challenge/lib/rock_challenge.ex
+++ elixir/rock_challenge/lib/rock_challenge.ex
@@ -15,4 +15,14 @@ defmodule RockChallenge do
   def hello do
     :world
   end
+  defp handle_file_read({ok, array}) do
+    IO.puts(array)
+    result =
+      array
+      |> Enum.map(fn item -> IO.puts(item) end)
+
+    {:ok, result}
+  end
+
+  defp handle_file_read({:error, reason}), do: {:error, "Error reading the file: #{reason}"}
 end
