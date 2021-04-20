c0451788818bf0328bf983c81629a5dda8e3b4d1
Sérgio D. Neto <sergio.deusdedith@gmail.com>
2021-04-19 23:03:58 -0300

    study: add solution of "Word Count" of exercism

---
+++ elixir/exercism/word-count/lib/word_count.ex
@@ -0,0 +1,11 @@
+defmodule WordCount do
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t()) :: map
+  def count(sentence) do
+    Enum.frequencies(~w{#{sentence}})
+  end
+end
