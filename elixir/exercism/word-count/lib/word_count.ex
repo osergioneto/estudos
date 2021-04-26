e686305a34d7117e72624b754992f12c7ed16d92
Sérgio D. Neto <sergio.deusdedith@gmail.com>
2021-04-26 17:50:12 -0300

    study: make word count deal with special chars

--- elixir/exercism/word-count/lib/word_count.ex
+++ elixir/exercism/word-count/lib/word_count.ex
@@ -6,6 +6,18 @@ defmodule WordCount do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
-    Enum.frequencies(~w{#{sentence}})
+    sentence
+    |> String.downcase()
+    |> clean_input()
+    |> Enum.frequencies()
+  end
+
+  defp clean_input(sentence) do
+    sentence
+    # Remove special characters
+    |> String.replace(~r{(!|!|&|@|\$|%|\^|&|,|:)}, "")
+    # Turn consecutive whitespaces into one
+    |> String.replace(~r{\s+}, " ")
+    |> String.split(~r{( |_)})
   end
 end
