5056564d0aa7c0bc49abeaaa840b4913a2a7a1d2
Sérgio D. Neto <sergio.deusdedith@gmail.com>
2021-05-24 23:32:32 -0300

    study: add simple recursion examples of sum between numbers and gcd

---
+++ elixir/book_programming_elixir_1_6/lib/chapters/6_modules_and_named_functions/function_calls_and_pattern_matching.ex
@@ -0,0 +1,28 @@
+defmodule SimpleRecursion do
+  @doc """
+  Sum all numbers between 1 and n.
+
+    iex> SimpleRecursion.sum(1)
+    1
+
+    iex> SimpleRecursion.sum(3)
+    6
+
+    iex> SimpleRecursion.sum(5)
+    15
+  """
+  def sum(1), do: 1
+  def sum(n), do: sum(n - 1) + n
+
+  @doc """
+  Return gcd(greatest common divisor) between two numbers.
+
+    iex> SimpleRecursion.gcd(2,12)
+    2
+
+    iex> SimpleRecursion.gcd(12,30)
+    6
+  """
+  def gcd(x, y) when y == 0, do: x
+  def gcd(x, y), do: gcd(y, rem(x, y))
+end
