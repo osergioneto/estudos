1afbbb0e9abead09814c8334b359cc43827116e6
Sérgio D. Neto <sergio.deusdedith@gmail.com>
2021-06-24 00:45:05 -0300

    study: add timer to license page

--- elixir/pragstudio-liveview-code/live_view_studio/lib/live_view_studio_web/live/license_live.ex
+++ elixir/pragstudio-liveview-code/live_view_studio/lib/live_view_studio_web/live/license_live.ex
@@ -1,11 +1,22 @@
 defmodule LiveViewStudioWeb.LicenseLive do
   use LiveViewStudioWeb, :live_view
   use Number
+  use Timex
 
+  alias Phoenix.LiveView.Socket
   alias LiveViewStudio.Licenses
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, seats: 2, amount: 60)}
+    if connected?(socket), do: :timer.send_interval(1000, self(), :tick)
+    expiration_time = Timex.shift(Timex.now(), minutes: 1)
+
+    {:ok,
+     assign(socket,
+       seats: 2,
+       amount: Licenses.calculate(2),
+       expiration_time: expiration_time,
+       time_remaining: time_remaining(expiration_time)
+     )}
   end
 
   def render(assigns) do
@@ -29,6 +40,16 @@ defmodule LiveViewStudioWeb.LicenseLive do
           </div>
         </div>
       </div>
+
+      <%= if @time_remaining > 0 do %>
+        <div class="card mt-2">
+          <div class="content">
+            <div class="amount">
+              <%= format_time(@time_remaining) %>
+            </div>
+          </div>
+        </div>
+      <% end %>
     </div>
     """
   end
@@ -38,7 +59,21 @@ defmodule LiveViewStudioWeb.LicenseLive do
     {:noreply, assign(socket, amount: Licenses.calculate(seats), seats: seats)}
   end
 
+  def handle_info(:tick, %Socket{assigns: %{expiration_time: expiration_time}} = socket) do
+    {:noreply, assign(socket, time_remaining: time_remaining(expiration_time))}
+  end
+
   defp singularize(seats) do
     if seats == 1, do: "seat", else: "seats"
+  end
+
+  defp time_remaining(expiration_time) do
+    DateTime.diff(expiration_time, Timex.now())
+  end
+
+  defp format_time(time) do
+    time
+    |> Timex.Duration.from_seconds()
+    |> Timex.format_duration(:humanized)
   end
 end
