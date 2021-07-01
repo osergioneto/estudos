6094397cf96a64683f23d796b026b16839fbcf85
Sérgio D. Neto <sergio.deusdedith@gmail.com>
2021-06-30 23:13:24 -0300

    study: add range on update time to sales dashboard

--- elixir/pragstudio-liveview-code/live_view_studio/lib/live_view_studio_web/live/sales_dashboard.ex
+++ elixir/pragstudio-liveview-code/live_view_studio/lib/live_view_studio_web/live/sales_dashboard.ex
@@ -5,9 +5,13 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    if connected?(socket), do: :timer.send_interval(1000, self(), :refresh)
+    socket =
+      socket
+      |> assign_stats()
+      |> assign(refresh: 1)
 
-    socket = assign(socket, get_values)
+    if connected?(socket), do: schedule_refresh(socket)
+
     {:ok, socket}
   end
 
@@ -42,25 +46,54 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
             </span>
           </div>
       </div>
-      <button phx-click="refresh"><img src="images/refresh.svg">Refresh</button>
+
+      <div class="controls">
+        <form phx-change="select-refresh">
+          <label for="refresh">
+            Refresh every:
+          </label>
+          <select name="refresh">
+            <%= options_for_select(refresh_options(), @refresh) %>
+          </select>
+        </form>
+        <button phx-click="refresh"><img src="images/refresh.svg">Refresh</button>
+      </div>
+
     </div>
     """
   end
 
-  defp get_values do
-    [
+  defp assign_stats(socket) do
+    assign(socket,
       new_orders: Sales.new_orders(),
       sales_amount: Sales.sales_amount(),
       satisfaction: Sales.satisfaction()
-    ]
+    )
   end
 
   @impl true
   def handle_event("refresh", _, socket) do
-    {:noreply, assign(socket, get_values)}
+    {:noreply, assign_stats(socket)}
   end
 
-  def handle_info(:refresh, socket) do
-    {:noreply, assign(socket, get_values)}
+  def handle_event("select-refresh", %{"refresh" => refresh}, socket) do
+    refresh = String.to_integer(refresh)
+    socket = assign(socket, refresh: refresh)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(:tick, socket) do
+    socket = assign_stats(socket)
+    schedule_refresh(socket)
+    {:noreply, socket}
+  end
+
+  defp refresh_options do
+    [{"1s", 1}, {"5s", 5}, {"15s", 15}, {"30s", 30}, {"60s", 60}]
+  end
+
+  defp schedule_refresh(socket) do
+    Process.send_after(self(), :tick, socket.assigns.refresh * 1000)
   end
 end
