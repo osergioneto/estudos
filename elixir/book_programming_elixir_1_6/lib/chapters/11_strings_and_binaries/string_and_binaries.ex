98946f1d61ee4d91bc3c8fd3729409b40e79960d
Sérgio Neto <sergio.deusdedith@gmail.com>
2022-02-11 21:04:27 -0300

    study: add answer to StringsAndBinaries-6

--- elixir/book_programming_elixir_1_6/lib/chapters/11_strings_and_binaries/string_and_binaries.ex
+++ elixir/book_programming_elixir_1_6/lib/chapters/11_strings_and_binaries/string_and_binaries.ex
@@ -45,4 +45,20 @@ defmodule StringAndBinaries do
 
     for word <- reduced, do: IO.puts(word)
   end
+
+  @doc """
+  ➤ Exercise: StringsAndBinaries-6
+    Write a function to capitalize the sentences in a string. Each sentence is terminated by a period and a space.
+    Right now, the case of the characters in the string is random.
+
+    iex> capitalize_sentences("oh. a DOG. woof. ") "Oh. A dog. Woof. "
+  """
+  def capitalize_sentences(string) do
+    string
+    |> String.split(". ")
+    |> Enum.map(&String.capitalize/1)
+    |> Enum.join(". ")
+  end
 end
+
+StringAndBinaries.capitalize_sentences("oh. a DOG. woof. ")
