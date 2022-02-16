d19c05cd489df4fb9988de9eacc89763ab074710
Sérgio Neto <sergio.deusdedith@gmail.com>
2022-02-15 22:06:05 -0300

    study: add IssusCLI module

---
+++ elixir/book_programming_elixir_1_6/lib/chapters/13_organizing_a_project/issues/lib/issues/cli.ex
@@ -0,0 +1,50 @@
+defmodule Issues.CLI do
+  @moduledoc """
+  Handle the command line parsing and the dispatch to the various functions that end up generating a table of the last _n_ issues in a github project
+  """
+
+  @default_count 4
+
+  def run(argv) do
+    argv
+    |> parse_args()
+    |> process()
+  end
+
+  @doc """
+  `argv` can be -h or --help, which returns :help.
+    Otherwise it is a github user name, project name, and (optionally)
+    the number of entries to format.
+  Return a tuple of `{ user, project, count }`, or `:help` if help was given.
+  """
+  def parse_args(argv) do
+    argv
+    |> OptionParser.parse(switches: [help: :boolean], aliases: [h: :help])
+    |> elem(1)
+    |> args_to_internal_representation()
+  end
+
+  def args_to_internal_representation([user, project, count]) do
+    {user, project, String.to_integer(count)}
+  end
+
+  def args_to_internal_representation([user, project]) do
+    {user, project, @default_count}
+  end
+
+  def args_to_internal_representation(_) do
+    :help
+  end
+
+  def process(:help) do
+    IO.puts("""
+    usage: issues <user> <project> [count | #{@default_count}]
+    """)
+
+    System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+  end
+end
