b01d2e3dbd2502b5563241c5ca0899cd92630793
Sérgio Neto <sergio.deusdedith@gmail.com>
2022-04-17 23:44:12 -0300

    study: nodes_2

---
+++ elixir/book_programming_elixir_1_6/lib/chapters/16_nodes/node_2.exs
@@ -0,0 +1,13 @@
+defmodule Nodes2 do
+  @doc """
+  ➤ Exercise: Nodes-2
+  When I introduced the interval server, I said it sent a tick “about every 2 seconds.”
+  But in the receive loop, it has an explicit timeout of 2,000 ms.
+  Why did I say “about” when it looks as if the time should be pretty accurate?
+
+  Resposta: O tempo de resposta do "tick" é por volta de 2 segundos pois
+  a função generator também trata os casos de registro.
+  Podem haver casos que o tempo de enviar as mensagens de tick chegou
+  porém o processamento de registro de um novo cliente ainda não terminou.
+  """
+end
