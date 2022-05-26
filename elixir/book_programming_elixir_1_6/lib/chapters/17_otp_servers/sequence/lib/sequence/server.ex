0c9c627f3708023989a86da14ec7b4ca98e0d1f0
Sérgio Neto <sergio.deusdedith@gmail.com>
2022-05-26 00:33:58 -0300

    study: adiciona format_status/2

--- elixir/book_programming_elixir_1_6/lib/chapters/17_otp_servers/sequence/lib/sequence/server.ex
+++ elixir/book_programming_elixir_1_6/lib/chapters/17_otp_servers/sequence/lib/sequence/server.ex
@@ -13,4 +13,8 @@ defmodule Sequence.Server do
   def handle_cast({:increment_number, delta}, current_number) do
     {:noreply, Impl.increment(current_number, delta)}
   end
+
+  def format_status(_reason, [_pdict, state]) do
+    [date: [{'State', "My current state is '#{inspect(state)}', and I'm happy"}]]
+  end
 end
