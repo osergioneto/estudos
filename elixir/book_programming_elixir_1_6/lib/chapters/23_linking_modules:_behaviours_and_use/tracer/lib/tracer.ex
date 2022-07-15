9bd02c8b9fb4ba67a171725336df7fe70ff1b7c7
Sérgio Neto <sergio.deusdedith@gmail.com>
2022-07-15 00:47:39 -0300

    study: behaviours and use

---
+++ elixir/book_programming_elixir_1_6/lib/chapters/23_linking_modules:_behaviours_and_use/tracer/lib/tracer.ex
@@ -0,0 +1,59 @@
+defmodule Tracer do
+  @moduledoc """
+  Documentation for `Tracer`.
+
+  ➤ Exercise: LinkingModules-BehavioursAndUse-1
+  In the body of the def macro, there’s a quote block that defines the actual method. It contains
+
+  IO.puts "==> call: #{Tracer.dump_dfn(unquote(name), unquote(args))}" result = unquote(content)
+  IO.puts "<== result: #{result}"
+
+  Why does the first call to puts have to unquote the values in its interpolation but the second call does not?
+  A: O primeiro block recebe valores que quando recebidos dentro da função irão expandir para visão de tupla de três valores, o unquote serve
+  para mostrar o valor "evaluated". Já o segundo o block recebe o resultado da operação, que na maioria das vezes será representado que é representado
+  por ele mesmo.
+
+  ➤ Exercise: LinkingModules-BehavioursAndUse-2
+  The built-in module IO.ANSI defines functions that represent ANSI escape sequences.
+  You can use it to build output than will display (for example) colors and bold,
+  inverse, or underlined text (assuming the terminal supports it).
+
+  iex> import IO.ANSI
+  iex> IO.puts [ "Hello, ", white(), green_background(), "world!"]
+  Hello, world!
+
+  Explore the module, and use it to colorize our tracing’s output.
+
+  Why does passing a list of strings to IO.puts work?
+  A: Na definição de tipos da função IO.puts, o segundo argumento, pode ser de dois tipos: chardata() | String.Chars.t(). O tipo
+  chardata() comporta um tipo chamado maybe_improper_list(), dando a inforção que o o argumento pode ser uma lista impropria.
+  """
+
+  IO.puts([green_background(), "Hello ", "world!", white_background()])
+
+  def dump_args(args) do
+    args |> Enum.map(&inspect/1) |> Enum.join(", ")
+  end
+
+  def dump_defn(name, args) do
+    "#{name}(#{dump_args(args)})"
+  end
+
+  defmacro def(definition = {name, _, args}, do: content) do
+    quote do
+      Kernel.def unquote(definition) do
+        IO.puts("==> call: #{Tracer.dump_defn(unquote(name), unquote(args))}")
+        result = unquote(content)
+        IO.puts("<== result: #{result}")
+        result
+      end
+    end
+  end
+
+  defmacro __using__(_opts) do
+    quote do
+      import Kernel, except: [def: 2]
+      import unquote(__MODULE__), only: [def: 2]
+    end
+  end
+end
