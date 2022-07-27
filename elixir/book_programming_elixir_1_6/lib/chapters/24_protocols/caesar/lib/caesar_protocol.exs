d016c3eca3b12d28b73ff6a3949ff07400961031
Sérgio Neto <sergio.deusdedith@gmail.com>
2022-07-26 23:44:31 -0300

    study: implment caeser cipher as protocol

--- elixir/book_programming_elixir_1_6/lib/chapters/24_protocols/caesar/lib/caesar_protocol.exs
+++ elixir/book_programming_elixir_1_6/lib/chapters/24_protocols/caesar/lib/caesar_protocol.exs
@@ -1,58 +1,87 @@
+@moduledoc """
+➤ Exercise: Protocols-1
+A basic Caesar cypher consists of shifting the letters in a message by
+a fixed offset. For an offset of 1, for example, a will become b,
+b will become c, and z will become a. If the offset is 13, we have the ROT13 algorithm.
+
+Lists and binaries can be stringlike. Write a Caesar protocol that applies
+to both. It would include two functions: encrypt(string, shift) and rot13(string).
+"""
+
 defprotocol Caesar do
   @fallback_to_any true
   def encrypt(string, shift)
-  def rot13(string, 13)
+  def rot13(string)
 end
 
-# alphabet = %{1 => "a", 2 => "b", 3 => "c"}
+defimpl Caesar, for: [List, BitString] do
+  @alphabet [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ]
 
-alphabet = %{
-  1=> "a",
-  2=> "b",
-  3=> "c",
-  4=> "d",
-  5=> "e",
-  6=> "f",
-  7 => "g",
-  8 => "h",
-  9 => "i",
-  10 => "j",
-  11 => "k",
-  12 => "l",
-  13 => "m",
-  14 => "n",
-  15 => "o",
-  16 => "p",
-  17 => "q",
-  18 => "r",
-  19 => "s",
-  20 => "t",
-  21 => "u",
-  22 => "v",
-  23 => "w",
-  24 => "x",
-  25 => "y",
-  26 => "z"
-}
-
-defimpl Caesar, for=>  [List, BitString] do
   def encrypt(string, shift) do
-
+    string
+    |> String.split("")
+    |> Enum.filter(fn letter -> letter != "" end)
+    |> Enum.reduce("", fn letter, acc -> shift_letter(letter, shift, acc) end)
   end
 
-  def get_shift_number(number, shift) when number + shift < 26 do
+  def rot13(string) do
+    encrypt(string, 13)
+  end
+
+  def shift_letter(letter, shift, acc) when letter not in [" "] do
+    shifted_letter =
+      letter
+      |> get_index_from_letter()
+      |> get_shift_number(shift)
+      |> get_letter_from_index()
+
+    acc <> shifted_letter
+  end
+
+  def shift_letter(letter, _, acc) do
+    acc <> letter
+  end
+
+  def get_shift_number(number, shift) when number + shift <= 25 do
     number + shift
   end
-  def get_shift_number(number, shift) when number + shift >= 26 do
-    get_shift_number(number - 26, shift)
-  end
-end
 
-defmodule Teste do
-  def get_shift_number(number, shift) when number + shift < 26 do
-    number + shift
-  end
-  def get_shift_number(number, shift) when number + shift >= 26 do
+  def get_shift_number(number, shift) when number + shift > 25 do
     get_shift_number(number - 26, shift)
+  end
+
+  def get_index_from_letter(letter) do
+    Enum.find_index(@alphabet, fn alphabet_letter -> alphabet_letter == letter end)
+  end
+
+  def get_letter_from_index(index) do
+    Enum.at(@alphabet, index)
   end
 end
--- elixir/book_programming_elixir_1_6/lib/chapters/24_protocols/caesar/lib/teste.ex
+++ elixir/book_programming_elixir_1_6/lib/chapters/24_protocols/caesar/lib/teste.ex
@@ -1,45 +1,71 @@
 defmodule Teste do
-  alphabet = %{
-    1 => "a",
-    2 => "b",
-    3 => "c",
-    4 => "d",
-    5 => "e",
-    6 => "f",
-    7 => "g",
-    8 => "h",
-    9 => "i",
-    10 => "j",
-    11 => "k",
-    12 => "l",
-    13 => "m",
-    14 => "n",
-    15 => "o",
-    16 => "p",
-    17 => "q",
-    18 => "r",
-    19 => "s",
-    20 => "t",
-    21 => "u",
-    22 => "v",
-    23 => "w",
-    24 => "x",
-    25 => "y",
-    26 => "z"
-  }
+  @alphabet [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ]
 
   def encrypt(string, shift) do
     string
     |> String.split("")
     |> Enum.filter(fn letter -> letter != "" end)
-    |> Enum.reduce("", fn letter, acc -> get_shift_number(shift) end)
+    |> Enum.reduce("", fn letter, acc -> shift_letter(letter, shift, acc) end)
   end
 
-  def get_shift_number(number, shift) when number + shift < 26 do
+  def rot13(string) do
+    encrypt(string, 13)
+  end
+
+  def shift_letter(letter, shift, acc) when letter not in [" "] do
+    shifted_letter =
+      letter
+      |> get_index_from_letter()
+      |> get_shift_number(shift)
+      |> get_letter_from_index()
+
+    acc <> shifted_letter
+  end
+
+  def shift_letter(letter, _, acc) do
+    acc <> letter
+  end
+
+  def get_shift_number(number, shift) when number + shift <= 25 do
     number + shift
   end
 
-  def get_shift_number(number, shift) when number + shift >= 26 do
+  def get_shift_number(number, shift) when number + shift > 25 do
     get_shift_number(number - 26, shift)
+  end
+
+  def get_index_from_letter(letter) do
+    Enum.find_index(@alphabet, fn alphabet_letter -> alphabet_letter == letter end)
+  end
+
+  def get_letter_from_index(index) do
+    Enum.at(@alphabet, index)
   end
 end
