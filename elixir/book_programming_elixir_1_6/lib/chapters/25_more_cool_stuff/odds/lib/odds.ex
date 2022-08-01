2c32b991156e99453057752432ef3e2387dab238
Sérgio Neto <sergio.deusdedith@gmail.com>
2022-07-31 22:34:18 -0300

    study: odds folder

---
+++ elixir/book_programming_elixir_1_6/lib/chapters/25_more_cool_stuff/odds/lib/odds.ex
@@ -0,0 +1,18 @@
+defmodule Odds do
+  @moduledoc """
+  Documentation for `Odds`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Odds.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
