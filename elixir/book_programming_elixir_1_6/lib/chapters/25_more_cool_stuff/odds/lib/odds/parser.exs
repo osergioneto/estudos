fcfee5601573f75733d1e41ecd0d5f30387214b0
Sérgio Neto <sergio.deusdedith@gmail.com>
2022-08-01 21:53:43 -0300

    study: sigil_v 2

--- elixir/book_programming_elixir_1_6/lib/chapters/25_more_cool_stuff/odds/lib/odds/parser.exs
+++ elixir/book_programming_elixir_1_6/lib/chapters/25_more_cool_stuff/odds/lib/odds/parser.exs
@@ -4,6 +4,17 @@ defmodule ParserSigil do
     |> String.split("\n")
     |> Enum.filter(&Kernel.!=(&1, ""))
     |> Enum.map(&String.split(&1, ","))
+    |> Enum.map(fn row ->
+      Enum.map(row, fn word -> parse_float(word) end)
+    end)
+  end
+
+  defp parse_float(word) do
+    case Float.parse(word) do
+      {number, ""} -> "#{number}"
+      {number, word} -> ["#{number}", "#{word}"]
+      :error -> word
+    end
   end
 
   defmacro __using__(_opts) do
@@ -19,10 +30,18 @@ defmodule Example2 do
 
   def teste do
     ~v"""
-    1,2,3
+    1,2,3.14
+    cat,dog
+    """
+  end
+
+  def teste2 do
+    ~v"""
+    1,2,3.14abc
     cat,dog
     """
   end
 end
 
 IO.inspect(Example2.teste())
+IO.inspect(Example2.teste2())
