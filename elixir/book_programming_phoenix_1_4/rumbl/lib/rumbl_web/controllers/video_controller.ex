0d9abecdea03373210cb1f6b1a7fe1c21e8afc2b
Sérgio Neto <sergio.deusdedith@gmail.com>
2022-09-03 19:45:14 -0300

    study(feat): add current user as paramers on all video_controller actions

--- elixir/book_programming_phoenix_1_4/rumbl/lib/rumbl_web/controllers/video_controller.ex
+++ elixir/book_programming_phoenix_1_4/rumbl/lib/rumbl_web/controllers/video_controller.ex
@@ -14,8 +14,8 @@ defmodule RumblWeb.VideoController do
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"video" => video_params}) do
-    case Multimedia.create_video(video_params) do
+  def create(conn, %{"video" => video_params}, current_user) do
+    case Multimedia.create_video(current_user, video_params) do
       {:ok, video} ->
         conn
         |> put_flash(:info, "Video created successfully.")
@@ -58,5 +58,10 @@ defmodule RumblWeb.VideoController do
     conn
     |> put_flash(:info, "Video deleted successfully.")
     |> redirect(to: Routes.video_path(conn, :index))
+  end
+
+  def action(conn, _) do
+    args = [conn, conn.params, conn.assigns.current_user]
+    apply(__MODULE__, action_name(conn), args)
   end
 end
