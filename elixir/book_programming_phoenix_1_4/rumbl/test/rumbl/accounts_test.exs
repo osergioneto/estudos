9d182a965d24103456cb020c4f9dbeb548c9a025
Sérgio Neto <sergio.deusdedith@gmail.com>
2022-09-26 00:03:26 -0300

    test: add tests for authenticate_by_username_and_pass/2

--- elixir/book_programming_phoenix_1_4/rumbl/test/rumbl/accounts_test.exs
+++ elixir/book_programming_phoenix_1_4/rumbl/test/rumbl/accounts_test.exs
@@ -46,4 +46,26 @@ defmodule Rumbl.AccountsTest do
     end
   end
 
+  describe "authenticate_by_username_and_pass/2" do
+    @pass "123456"
+
+    setup do
+      {:ok, user: user_fixture(password: @pass)}
+    end
+
+    test "returns user with correct password", %{user: user} do
+      assert {:ok, auth_user} = Accounts.authenticate_by_username_and_pass(user.username, @pass)
+      assert auth_user.id == user.id
+    end
+
+    test "return unauthorized error with invalid password", %{user: user} do
+      assert {:error, :unauthorized} =
+               Accounts.authenticate_by_username_and_pass(user.username, "badpass")
+    end
+
+    test "return not found error with no matching user for email" do
+      assert {:error, :not_found} =
+               Accounts.authenticate_by_username_and_pass("unknownuser", @pass)
+    end
+  end
 end
