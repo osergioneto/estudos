0bf4d38084a7fe4793b713c2b5aa9672421aa638
Sérgio Neto <sergio.deusdedith@gmail.com>
2022-10-22 22:55:26 -0300

    feat: create video channel

--- elixir/book_programming_phoenix_1_4/rumbl/lib/rumbl_web/channels/user_socket.ex
+++ elixir/book_programming_phoenix_1_4/rumbl/lib/rumbl_web/channels/user_socket.ex
@@ -1,6 +1,8 @@
 defmodule RumblWeb.UserSocket do
   use Phoenix.Socket
   # channel "room:*", RumblWeb.RoomChannel
+  channel "videos:*", RumblWeb.VideoChannel
+
   def connect(_params, socket, _connect_info) do
     {:ok, socket}
   end
