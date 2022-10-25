693471b8d6f8c6a66d489105857de04728701242
Sérgio Neto <sergio.deusdedith@gmail.com>
2022-10-24 23:15:17 -0300

    feat: validate user token on UserSocket

--- elixir/book_programming_phoenix_1_4/rumbl/lib/rumbl_web/channels/user_socket.ex
+++ elixir/book_programming_phoenix_1_4/rumbl/lib/rumbl_web/channels/user_socket.ex
@@ -3,9 +3,24 @@ defmodule RumblWeb.UserSocket do
   # channel "room:*", RumblWeb.RoomChannel
   channel "videos:*", RumblWeb.VideoChannel
 
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
+  @max_age 2 * 7 * 24 * 60 * 60
+
+  def connect(%{"token" => token}, socket, _connect_info) do
+    case Phoenix.Token.verify(
+           socket,
+           "user socket",
+           token,
+           max_age: @max_age
+         ) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+
+      {:error, _reason} ->
+        :error
+    end
   end
 
-  def id(_socket), do: nil
+  def connect(_params, _socket, _connect_info), do: :error
+
+  def id(socket), do: "users_socket:#{socket.assigns.user_id}"
 end
