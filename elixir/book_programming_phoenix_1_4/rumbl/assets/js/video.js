d8f866014978c4a384b1b4a94c7ab6120be345fc
Sérgio Neto <sergio.deusdedith@gmail.com>
2022-10-26 00:56:28 -0300

    feat: render annotations at the time that was first sent

--- elixir/book_programming_phoenix_1_4/rumbl/assets/js/video.js
+++ elixir/book_programming_phoenix_1_4/rumbl/assets/js/video.js
@@ -27,8 +27,18 @@ let Video = {
             this.renderAnnotation(msgContainer, resp)
         })
 
+        msgContainer.addEventListener("click", e => {
+            e.preventDefault()
+            let seconds = e.target.getAttribute("data-seek") ||
+                e.target.parentNode.getAttribute("data-seek")
+            if (!seconds) { return }
+            Player.seekTo(seconds)
+        })
+
         vidChannel.join()
-            .receive("ok", resp => console.log("joined the video channel", resp))
+            .receive("ok", resp => {
+                this.scheduleMessages(msgContainer, resp.annotations)
+            })
             .receive("error", reason => console.log("join failed", reason))
     },
     esc(str) {
@@ -47,7 +57,30 @@ let Video = {
 
         msgContainer.appendChild(template)
         msgContainer.scrollTop = msgContainer.scrollHeight
-    }
+    },
+    scheduleMessages(msgContainer, annotations) {
+        clearTimeout(this.scheduleTimer)
+        this.schedulerTimer = setTimeout(() => {
+            let ctime = Player.getCurrentTime()
+            let remaining = this.renderAtTime(annotations, ctime, msgContainer)
+            this.scheduleMessages(msgContainer, remaining)
+        }, 1000)
+    },
+    renderAtTime(annotations, seconds, msgContainer) {
+        return annotations.filter(ann => {
+            if (ann.at > seconds) {
+                return true
+            } else {
+                this.renderAnnotation(msgContainer, ann)
+                return false
+            }
+        })
+    },
+    formatTime(at) {
+        let date = new Date(null)
+        date.setSeconds(at / 1000)
+        return date.toISOString().substr(14, 5)
+    },
 }
 
 export default Video;
