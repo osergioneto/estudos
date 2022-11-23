51b592fab068a40a7949d414b1a090c48359f184
Sérgio Neto <sergio.deusdedith@gmail.com>
2022-11-22 23:22:47 -0300

    chore: add SweetXml and wolfram env

--- elixir/book_programming_phoenix_1_4/rumbl_umbrella/config/dev.exs
+++ elixir/book_programming_phoenix_1_4/rumbl_umbrella/config/dev.exs
@@ -29,6 +29,14 @@ config :rumbl_web, RumblWeb.Endpoint,
     esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
   ]
 
+wolfram_app_id =
+  System.get_env("WOLFRAM_APP_ID") ||
+    raise """
+    environment variable WOLFRAM_APP_ID is missing
+    """
+
+config :info_sys, :wolfram, app_id: wolfram_app_id
+
 # ## SSL Support
 #
 # In order to use HTTPS in development, a self-signed
