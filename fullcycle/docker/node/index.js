8a61d6f768e9d16f246426d4ae3072a77566e824
Sérgio Neto <sergio.deusdedith@gmail.com>
2023-07-03 10:23:02 -0300

    study: add file watching

--- fullcycle/docker/node/index.js
+++ fullcycle/docker/node/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello World!!</h1>");
+    res.send("<h1>Hello World!!!!!</h1>");
 });
 
 app.listen(PORT, () => console.log(`Server on port: ${PORT}`));
