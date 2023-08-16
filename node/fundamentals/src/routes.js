8323237fa544eb39c1152db6b9207968d3998782
Sérgio Neto <sergio.deusdedith@zig.fun>
2023-08-15 23:47:13 -0300

    study: crud

--- node/fundamentals/src/routes.js
+++ node/fundamentals/src/routes.js
@@ -1,21 +1,23 @@
 import { randomUUID } from "node:crypto"
 import { Database  } from "./database.js"
+import { buildRoutePath } from "./utils/build-route-path.js"
 
 const database = new Database()
 
 export const routes = [
     {
         method: 'GET',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (req, res) => {
-            const users = database.select('users')
+            const { search } = req.query
+            const users = database.select('users', search ? { name: search, email: search} : null)
 
             return res.end(JSON.stringify(users))
         }
     },
     {
         method: 'POST',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (req, res) => {
             const { name, email } = req.body
 
@@ -28,6 +30,29 @@ export const routes = [
             database.insert('users', user)
             
             return res.writeHead(201).end('Criação de usuário')
+        }
+    },
+    {
+        method: 'PUT',
+        path: buildRoutePath('/userd/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+            const { name, email }= req.body
+
+            database.update('users', id, { name, email })
+
+            return res.writeHead(204).end()
+        }
+    },
+    {
+        method: 'DELETE',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+
+            database.delete('users', id)
+
+            return res.writeHead(204).end()
         }
     }
 ]
