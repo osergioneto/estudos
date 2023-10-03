8680081a1214c509410f438ddaf984c5a61075e9
Sérgio Neto <sergio.deusdedith@zig.fun>
2023-10-02 23:03:05 -0300

    chore: app setup

---
+++ node/daily-diet-api/src/database.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import { knex as setupKnex, Knex } from 'knex'
+import { env } from './env'
+
+export const config: Knex.Config = {
+  client: 'sqlite',
+  connection: {
+    filename: env.DATABASE_URL,
+  },
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+  useNullAsDefault: true,
+}
+
+export const knex = setupKnex(config)
