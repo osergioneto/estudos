2b784d4b86fc19a3748b3c447e566650c9b166e7
Sérgio Neto <sergio.deusdedith@zig.fun>
2023-10-02 23:03:47 -0300

    feat: add route to create user

---
+++ node/daily-diet-api/src/routes/users.ts
@@ -0,0 +1,23 @@
+import { FastifyInstance } from 'fastify'
+import { knex } from '../database'
+import { z } from 'zod'
+import { randomUUID } from 'crypto'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const createUserBodySchema = z.object({
+      name: z.string(),
+      email: z.string(),
+    })
+
+    const { name, email } = createUserBodySchema.parse(request.body)
+
+    await knex('users').insert({
+      id: randomUUID(),
+      name,
+      email,
+    })
+
+    return reply.status(201).send()
+  })
+}
