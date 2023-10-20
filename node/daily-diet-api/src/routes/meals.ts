12be21b7fe7bee0de96b390e83f57e95fa2b4bde
Sérgio Neto <sergio.deusdedith@zig.fun>
2023-10-20 01:21:35 -0300

    feat: add endpoint to edit user

--- node/daily-diet-api/src/routes/meals.ts
+++ node/daily-diet-api/src/routes/meals.ts
@@ -45,4 +45,49 @@ export async function mealsRoutes(app: FastifyInstance) {
       return reply.status(201).send()
     },
   )
+
+  app.put(
+    '/:mealId',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const { sessionId } = request.cookies
+      const user = await knex('users').where('session_id', sessionId).first()
+      const { mealId } = request.params
+
+      const meal = await knex('meals').where('id', mealId).first()
+
+      if (!meal) {
+        return reply.status(404).send()
+      }
+
+      const updateMealBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        ateAt: z.coerce.date(),
+        onDiet: z.coerce.boolean(),
+      })
+
+      const result = updateMealBodySchema.safeParse(request.body)
+
+      if (!result.success) {
+        return reply.status(400).send(result.error.issues)
+      }
+
+      const {
+        data: { name, description, ateAt, onDiet },
+      } = result
+
+      const mealToUpdate = {
+        id: meal?.id,
+        name: name ?? meal?.name,
+        description: description ?? meal?.name,
+        ate_at: ateAt ?? meal?.ate_at,
+        on_diet: onDiet ?? meal?.on_diet,
+      }
+
+      await knex('meals').update(mealToUpdate)
+
+      return reply.status(200).send()
+    },
+  )
 }
