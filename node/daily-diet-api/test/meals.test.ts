b8f66d20885c78a2a3fe4e4dca853de48ffd8219
Sérgio Neto <sergio.deusdedith@zig.fun>
2023-10-21 23:55:24 -0300

    test: add test for update route

--- node/daily-diet-api/test/meals.test.ts
+++ node/daily-diet-api/test/meals.test.ts
@@ -2,6 +2,7 @@ import { expect, it, beforeAll, afterAll, describe, beforeEach } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 import { execSync } from 'child_process'
+import { knex } from '../src/database'
 
 beforeAll(async () => {
   await app.ready()
@@ -61,5 +62,41 @@ describe('Meals routes', () => {
         onDiet: 'false',
       })
       .expect(400)
+  })
+
+  it('should be able to update meal', async () => {
+    const user = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'John Doe',
+        email: 'john.doe@gmail.com',
+      })
+      .expect(201)
+
+    const cookies = user.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/meals/')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Sorvete de Pistache',
+        description: 'melhor sorvete',
+        ateAt: '2023-10-09 03:26:35',
+        onDiet: 'false',
+      })
+      .expect(201)
+
+    const createdMeal = await knex('meals').select().first()
+
+    await request(app.server)
+      .put(`/meals/${createdMeal?.id}`)
+      .set('Cookie', cookies)
+      .send({ name: 'Vatapá', description: 'melhor comida' })
+      .expect(200)
+
+    const updatedMeal = await knex('meals').select().first()
+
+    expect(createdMeal?.id).toEqual(updatedMeal?.id)
+    console.log('createdMeal: ', createdMeal)
   })
 })
