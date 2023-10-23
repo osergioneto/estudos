6b04e34bb52e77a3d3dd52682c44e813f3fb20a1
Sérgio Neto <sergio.deusdedith@zig.fun>
2023-10-23 18:12:58 -0300

    feat: add endpoint to get user metrics

--- node/daily-diet-api/src/routes/users.ts
+++ node/daily-diet-api/src/routes/users.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
 import { z } from 'zod'
 import { randomUUID } from 'crypto'
+import { checkSessionIdExists } from '../middleware/check-session-id-exists'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.get('/', async (request, reply) => {
@@ -38,4 +39,64 @@ export async function usersRoutes(app: FastifyInstance) {
 
     return reply.status(201).send()
   })
+
+  app.get(
+    '/metrics',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const { sessionId } = request.cookies
+      const user = await knex('users').where('session_id', sessionId).first()
+
+      if (!user) {
+        return reply.status(404).send({ message: "User doesn't exists" })
+      }
+
+      const mealsCount = knex('meals')
+        .count('* as count')
+        .where({ user_id: user?.id })
+
+      const mealsData = knex('meals')
+        .select('*')
+        .where({ user_id: user?.id })
+
+      const [[{ count }], meals] = await Promise.all([mealsCount, mealsData])
+
+      const { onDietCount, offDietCount } = meals.reduce(
+        (acc, meal) => {
+          meal.on_diet ? acc.onDietCount++ : acc.offDietCount++
+          return acc
+        },
+        { onDietCount: 0, offDietCount: 0 },
+      )
+
+      const onDietStreak = findLongestonDietStreak(meals)
+
+      const metrics = {
+        mealsCount: count,
+        onDietCount,
+        offDietCount,
+        onDietStreak
+      }
+
+      return reply.status(200).send(metrics)
+    },
+  )
+}
+
+function findLongestonDietStreak(meals) {
+  let longestSequence = 0
+  let currentSequence = 0
+
+  for (let i = 0; i < meals.length; i++) {
+    if (meals[i].on_diet === 1) {
+      currentSequence++
+      if (currentSequence > longestSequence) {
+        longestSequence = currentSequence
+      }
+    } else {
+      currentSequence = 0
+    }
+  }
+
+  return longestSequence
 }
