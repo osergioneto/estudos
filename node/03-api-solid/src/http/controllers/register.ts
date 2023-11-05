e7d0499434d2e85100450d9cca164adb1d53225a
Sérgio Neto <sergio.deusdedith@zig.fun>
2023-11-05 20:58:21 -0300

    refactor: extract routes and controllers

---
+++ node/03-api-solid/src/http/controllers/register.ts
@@ -0,0 +1,24 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { prisma } from '@/lib/prisma'
+import { request } from 'http'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+    const registerBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6),
+    })
+
+    const { email, name, password } = registerBodySchema.parse(request.body)
+
+    await prisma.user.create({
+        data: {
+            email,
+            name,
+            password_hash: password
+        }
+    })
+
+    return reply.status(201).send()
+}
