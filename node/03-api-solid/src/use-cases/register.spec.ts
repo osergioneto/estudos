0ac14d6a976f81fb45962a5c2ddb396798af67d1
Sérgio Neto <sergio.deusdedith@zig.fun>
2023-11-20 17:00:35 -0300

    test: check hash creation

--- node/03-api-solid/src/use-cases/register.spec.ts
+++ node/03-api-solid/src/use-cases/register.spec.ts
@@ -1,5 +1,34 @@
-import { test, expect } from 'vitest'
+import { describe, expect, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
 
-test('should check 2 + 2 = 4', () => {
-    expect(2 + 2).toBe(4)
+describe('Register Use Case', () => {
+    it('should hash user password upon registration', async () => {
+        const registerUseCase = new RegisterUseCase({
+            async create(data) {
+                return {
+                    id: 'user-1',
+                    name: data.name,
+                    email: data.email,
+                    password_hash: data.password_hash,
+                    created_at: new Date(),
+                    updated_at: new Date(),
+                }
+            },
+
+            async findByEmail(email) {
+                return null
+            }
+        })
+
+        const { user } = await registerUseCase.execute({
+            name: 'John Doe',
+            email: 'john.doe@example.com',
+            password: '123456'
+        })
+
+        const isPasswordCorrectlyHashed = await compare('123456', user.password_hash)
+
+        expect(isPasswordCorrectlyHashed).toBeTruthy()
+    })
 })
