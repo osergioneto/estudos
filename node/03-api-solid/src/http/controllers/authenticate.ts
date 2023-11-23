cd5b1c2d2fa5eacc85058058e8912e4c338b42e8
Sérgio Neto <sergio.deusdedith@zig.fun>
2023-11-22 23:54:15 -0300

    refactor: add function to make repositories

--- node/03-api-solid/src/http/controllers/authenticate.ts
+++ node/03-api-solid/src/http/controllers/authenticate.ts
@@ -1,8 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
-import { AuthenticateUseCase } from '@/use-cases/authenticate'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { UserAlreadyExistisError } from '@/use-cases/errors/user-already-exists-error'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 
 export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
     const authenticateBodySchema = z.object({
@@ -13,9 +12,7 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
     const { email, password } = authenticateBodySchema.parse(request.body)
 
     try {
-        const prismaUsersRepository = new PrismaUsersRepository()
-        const authenticateUseCase = new AuthenticateUseCase(prismaUsersRepository)
-
+        const authenticateUseCase = makeAuthenticateUseCase()
         await authenticateUseCase.execute({ email, password })
     } catch (error) {
         if (error instanceof UserAlreadyExistisError) {
