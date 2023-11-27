8d97240b864269db05cbc47d0c2b45357664a839
Sérgio Neto <sergio.deusdedith@zig.fun>
2023-11-26 22:14:21 -0300

    feat: add use case to fetch check ins history

---
+++ node/03-api-solid/src/use-cases/fetch-user-check-ins-history.spec.ts
@@ -0,0 +1,37 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { FetchUserCheckInsUseCase } from './fetch-user-check-ins-history'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: FetchUserCheckInsUseCase
+
+describe('Check In Use Case', () => {
+    beforeEach(async () => {
+        checkInsRepository = new InMemoryCheckInsRepository()
+        sut = new FetchUserCheckInsUseCase(checkInsRepository)
+
+
+    })
+
+    it('should be able to fetch check-ins history', async () => {
+        for (let i = 1; i <= 22; i++) {
+            await checkInsRepository.create({
+                user_id: 'user-id',
+                gym_id: `gym-${i}`,
+                created_at: new Date(),
+                updated_at: null
+            })
+        }
+
+
+        const { checkIns } = await sut.execute({
+            userId: 'user-id',
+            page: 2
+        })
+
+        console.log("checkIns", checkIns)
+
+        expect(checkIns).toHaveLength(2)
+    })
+})
+
