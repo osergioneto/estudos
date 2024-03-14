2b83e74d7058673bbcf171a70e0c21db05bf477a
Sérgio Neto <sergio.deusdedith@zig.fun>
2024-03-13 21:46:53 -0300

    study: add tests of register

--- node/03-api-solid/src/http/controllers/register.spec.ts
+++ node/03-api-solid/src/http/controllers/register.spec.ts
@@ -1,3 +1,23 @@
-import { test } from "vitest";
+import request from "supertest"
+import { describe, it, expect, beforeAll, afterAll } from "vitest"
+import { app } from "@/app"
 
-test('ok', () => { })
+describe('Register (e2e)', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('should be able to register user', async () => {
+        const response = await request(app.server).post('/users').send({
+            name: 'John Doe',
+            email: 'johndoe@email.com',
+            password: '123456'
+        })
+
+        expect(response.status).toBe(201)
+    })
+})
