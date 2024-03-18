d34834faa04447de375b1c99151732ca1d25dc95
Sérgio Neto <sergio.deusdedith@zig.fun>
2024-03-17 23:56:04 -0300

    study: add nearby endpoint

---
+++ node/03-api-solid/src/http/controllers/gyms/nearby.spec.ts
@@ -0,0 +1,57 @@
+import request from "supertest"
+import { describe, it, expect, beforeAll, afterAll } from "vitest"
+import { app } from "@/app"
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user"
+
+describe('Nearby Gyms (e2e)', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('should be able to list nearby gym', async () => {
+        const { token } = await createAndAuthenticateUser(app)
+
+        await request(app.server)
+            .post('/gyms')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                name: 'SmartFit',
+                description: "Venha treinar na maior rede de academias da América Latina",
+                phone: "(34) 3433-9402",
+                latitude: -27.2092052,
+                longitude: -49.6401092,
+            })
+
+        await request(app.server)
+            .post('/gyms')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                name: 'gym',
+                description: "Venha treinar na maior rede de academias da América Latina",
+                phone: "(34) 3433-9402",
+                latitude: -23.5916201,
+                longitude: -46.6222736,
+            })
+
+        const response = await request(app.server)
+            .get('/gyms/nearby')
+            .query({
+                latitude: -27.2092052,
+                longitude: -49.6401092
+            })
+            .set('Authorization', `Bearer ${token}`)
+            .send()
+
+        expect(response.statusCode).toBe(200)
+        expect(response.body.gyms).toHaveLength(1)
+        expect(response.body.gyms).toEqual([
+            expect.objectContaining({
+                name: 'SmartFit'
+            })
+        ])
+    })
+})
