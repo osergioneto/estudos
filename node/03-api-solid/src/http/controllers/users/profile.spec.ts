3d3c48abe0902c2722743f7313aeac32b95512ec
Sérgio Neto <sergio.deusdedith@zig.fun>
2024-03-17 23:57:21 -0300

    study: add profiles endpoint

---
+++ node/03-api-solid/src/http/controllers/users/profile.spec.ts
@@ -0,0 +1,32 @@
+import request from "supertest"
+import { describe, it, expect, beforeAll, afterAll } from "vitest"
+import { app } from "@/app"
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user"
+
+describe('Profile (e2e)', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('should be able to get profile', async () => {
+        const { token } = await createAndAuthenticateUser(app)
+
+        const profile = await request(app.server).get('/me').set('Authorization', `Bearer ${token}`)
+
+        expect(profile.status).toBe(200)
+        expect(profile.body).toEqual({
+            user: {
+                id: expect.any(String),
+                name: 'John Doe',
+                email: 'johndoe@email.com',
+                password_hash: expect.any(String),
+                created_at: expect.any(String),
+                updated_at: expect.any(String)
+            }
+        })
+    })
+})
