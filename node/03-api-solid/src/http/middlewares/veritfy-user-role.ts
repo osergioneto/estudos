8b243fe699a009f7cd3e0cfb46bf3d6bd2cb2969
Sérgio Neto <sergio.deusdedith@zig.fun>
2024-03-20 22:42:53 -0300

    study: rename variable

--- node/03-api-solid/src/http/middlewares/veritfy-user-role.ts
+++ node/03-api-solid/src/http/middlewares/veritfy-user-role.ts
@@ -1,8 +1,8 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 
-export function verifyUserRole(role: 'ADMIN' | 'MEMBER') {
+export function verifyUserRole(roleToVerify: 'ADMIN' | 'MEMBER') {
     return async (request: FastifyRequest, reply: FastifyReply) => {
-        if (role !== request.user.role) {
+        if (roleToVerify !== request.user.role) {
             return reply.status(401).send({ message: 'Unauthorized' })
         }
     }
