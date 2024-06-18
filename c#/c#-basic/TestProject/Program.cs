7a68209790db5829d003607fa835f2c219aa37de
Sérgio Neto <sergio.deusdedith@zig.fun>
2024-06-18 11:58:55 -0300

    study: elseif and switch

--- c#/c#-basic/TestProject/Program.cs
+++ c#/c#-basic/TestProject/Program.cs
@@ -1,5 +1,36 @@
-﻿int firstValue = 500;
-int secondValue = 600;
-int largerValue = Math.Max(firstValue, secondValue);
+﻿Random dice = new Random();
 
-Console.WriteLine(largerValue);
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+int extraPoints = 0;
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        extraPoints += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        extraPoints += 2;
+    }
+}
+
+
+int total = roll1 + roll2 + roll3 + extraPoints;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} + {extraPoints} = {total}");
+
+string result = total switch
+{
+    >= 16 => "You win a new car!",
+    >= 10 => "You win a new laptop!",
+    7 => "You win a trip for two!",
+    _ => "You win a kitten!"
+};
+
+
+Console.WriteLine(result);
