11417f26ea579d3c856dd891c25e3c499b8e1e51
Sérgio Neto <sergio.deusdedith@zig.fun>
2024-06-18 12:18:01 -0300

    study(exercise): subscription string

--- c#/c#-basic/TestProject/Program.cs
+++ c#/c#-basic/TestProject/Program.cs
@@ -1,36 +1,14 @@
-﻿Random dice = new Random();
-
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
-int extraPoints = 0;
-
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+// int discountPercentage = 0;
+Console.WriteLine($"daysUntilExpiration: {daysUntilExpiration}");
+string message = daysUntilExpiration switch
 {
-    if ((roll1 == roll2) && (roll2 == roll3))
-    {
-        Console.WriteLine("You rolled triples!  +6 bonus to total!");
-        extraPoints += 6;
-    }
-    else
-    {
-        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-        extraPoints += 2;
-    }
-}
-
-
-int total = roll1 + roll2 + roll3 + extraPoints;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} + {extraPoints} = {total}");
-
-string result = total switch
-{
-    >= 16 => "You win a new car!",
-    >= 10 => "You win a new laptop!",
-    7 => "You win a trip for two!",
-    _ => "You win a kitten!"
+    0 => "Your subscription has expired.",
+    1 => $"Your subscription expires within a day!\n Renew now and save 20%!",
+    < 5 => $"Your subscription expires in {daysUntilExpiration} days. \nRenew now and save 10%!",
+    < 10 => "Your subscription will expire soon. Renew now!",
+    _ => ""
 };
 
-
-Console.WriteLine(result);
+Console.WriteLine(message);
