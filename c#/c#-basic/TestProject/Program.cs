28466ddbf3db73ac60446aa2cd7f17ccec60a611
Sérgio Neto <sergio.deusdedith@zig.fun>
2024-06-18 21:35:32 -0300

    study: foreach

--- c#/c#-basic/TestProject/Program.cs
+++ c#/c#-basic/TestProject/Program.cs
@@ -1,20 +1,13 @@
-﻿/*
-string[] fraudulentOrderIDs = new string[3];
+﻿int[] inventory = { 200, 450, 700, 175, 250 };
+int total = 0;
+int bin = 0;
 
-fraudulentOrderIDs[0] = "A123";
-fraudulentOrderIDs[1] = "B456";
-fraudulentOrderIDs[2] = "C789";
-// fraudulentOrderIDs[3] = "D000";
-*/
+foreach (int items in inventory)
+{
+    bin++;
+    total += items;
 
-string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {total})");
+}
 
-Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
-Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
-Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
-
-fraudulentOrderIDs[0] = "F000";
-
-Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
-
-Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+Console.WriteLine($"We have {total} items in inventory.");
