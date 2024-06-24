cce43a50f39c526682c9b32048c7fe39b9690c32
Sérgio Neto <sergio.deusdedith@zig.fun>
2024-06-24 18:39:06 -0300

    study: pattern matching and ternary

--- c#/c#-basic/TestProject/Program.cs
+++ c#/c#-basic/TestProject/Program.cs
@@ -1,4 +1,19 @@
-Random dice = new Random();
-int number = dice.Next(1, 3);
-string coin = number == 1 ? "heads" : "tails";
-Console.WriteLine(coin);
+using System.Data;
+using System.Text.RegularExpressions;
+
+string permission = "Manager";
+string pattern = @"\b(Admin|Manager)\b";
+int level = 55;
+
+Match match = Regex.Match(permission, pattern);
+
+string message = (match.Value, level) switch
+{
+    ("Admin", > 55) => "Welcome, Super Admin user.",
+    ("Admin", <= 55) => "Welcome, Admin user.",
+    ("Manager", >= 20) => "Contact an Admin for access.",
+    ("Manager", <= 20) => "You do not have sufficient privileges.",
+    _ => "You do not have sufficient privileges."
+};
+
+Console.WriteLine(message);
