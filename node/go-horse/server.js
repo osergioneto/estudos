7bdd1697103dc9fff0720e1719e85e4c21c6b4db
Sérgio Neto <sergio.deusdedith@zig.fun>
2024-09-04 23:04:52 -0300

    study: presentation

---
+++ node/go-horse/server.js
@@ -0,0 +1,11 @@
+// server.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Dunder Mifflin Online!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
