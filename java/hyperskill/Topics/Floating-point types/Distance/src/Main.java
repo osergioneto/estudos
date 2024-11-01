b4e37880a941c419c6610493657e1c4f11ea0812
Sérgio Neto <sergio.deusdedith@zig.fun>
2024-10-31 23:58:52 -0300

    study: java

---
+++ java/hyperskill/Topics/Floating-point types/Distance/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double distance = scanner.nextDouble();
+        double time = scanner.nextDouble();
+
+        double average = distance / time;
+
+        System.out.println(average);
+    }
+}
