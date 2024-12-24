a7d72385d8a4251b9af494c44a9524c369b720a3
Sérgio Neto <sergio.deusdedith@zig.fun>
2024-12-23 23:15:09 -0300

    study: go

---
+++ go/GolandProjects/Coding a 1D array/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Create the array of 10 elements and assign 54 to its [4] element below:
+	array := [10]int{4: 54}
+
+	// DO NOT delete, this line prints the array!
+	fmt.Println(array)
+}
