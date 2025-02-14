fe152226e084be042ab0e71b9bd6679ba05f658b
Sérgio Neto <sergio.deusdedith@zig.fun>
2025-02-14 00:35:42 -0300

    study: leetcode

--- leetcode/412_fizz_buzz.go
+++ leetcode/412_fizz_buzz.go
@@ -6,18 +6,28 @@ import (
 )
 
 func main() {
-	n := 3
+	n := 35
 	answer := make([]string, n)
 
 	for i := 0; i < n; i++ {
 		number := i + 1
-		if number%3 == 0 && number%5 == 0 {
-			answer[i] = "FizzBuzz"
-		} else if number%3 == 0 {
-			answer[i] = "Fizz"
-		} else if number%5 == 0 {
-			answer[i] = "Buzz"
-		} else {
+		divisibleBy3 := number%3 == 0
+		divisibleBy5 := number%5 == 0
+		divisibleBy7 := number%7 == 0
+
+		if divisibleBy3 {
+			answer[i] += "Fizz"
+		}
+
+		if divisibleBy5 {
+			answer[i] += "Buzz"
+		}
+
+		if divisibleBy7 {
+			answer[i] += "Jazz"
+		}
+
+		if answer[i] == "" {
 			answer[i] = strconv.FormatInt(int64(number), 10)
 		}
 	}
