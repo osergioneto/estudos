a63177dabea44dda6c566051104a10b59e030929
Sérgio Deusdedith de Araujo Neto <sergio.deusdedith@gmail.com>
2025-08-12 01:13:49 +0000

    study: go

---
+++ go/go_the_the_complete_developers_guide/helloworld/check/check.go
@@ -0,0 +1,11 @@
+package check
+
+import "fmt"
+
+func MineTest() {
+	fmt.Println("test main")
+}
+
+func NewMineTest() {
+	fmt.Println("new test")
+}
---
+++ go/go_the_the_complete_developers_guide/helloworld/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"example.com/check"
+)
+
+func main() {
+	check.MineTest()
+	// fmt.Println("Hi there!")
+}
