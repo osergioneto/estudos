dcb61f0dd219e291b099f58169ab807eec9f6fe4
Sérgio Deusdedith de Araujo Neto <sergio.deusdedith@gmail.com>
2025-08-14 20:44:37 +0000

    study: receiver functions

--- go/go_the_the_complete_developers_guide/cards/main.go
+++ go/go_the_the_complete_developers_guide/cards/main.go
@@ -1,13 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
-	card := newCard()
+	cards := deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
 
-	fmt.Println(card)
+	cards.print()
 }
 
 func newCard() string {
-	return "Five of Spades"
+	return "Five of Diamonds"
 }
