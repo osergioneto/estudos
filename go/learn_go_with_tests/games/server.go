9373bdf5365d99103a1aa918460d682f0830183f
Sérgio Neto <sergio.deusdedith@gmail.com>
2025-11-24 23:54:09 -0300

    study go

---
+++ go/learn_go_with_tests/games/server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	fmt.Println("player: ", player)
+
+	if player == "Pepper" {
+		fmt.Fprint(w, "20")
+		return
+	}
+
+	if player == "Floyd" {
+		fmt.Fprint(w, "10")
+		return
+	}
+}
